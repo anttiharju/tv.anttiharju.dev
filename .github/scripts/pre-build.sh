7c010d74ae3ad83209a7bf0af8e31842825346ad
Antti Harju <antti@harju.io>
2024-08-18 14:09:03 +0300

    Try using src dir

--- .github/scripts/pre-build.sh
+++ .github/scripts/pre-build.sh
@@ -2,11 +2,11 @@
 set -eu
 
 # Read playlist names from playlists.yml
-playlists=$(grep -E '^[a-zA-Z0-9_-]+:$' playlists.yml | sed 's/://')
+playlists=$(grep -E '^[a-zA-Z0-9_-]+:$' src/playlists.yml | sed 's/://')
 
 # Copy index.html for each playlist
 for playlist in $playlists; do
   if [ "$playlist" != "default" ]; then
-    cp index.html "$playlist.html"
+    cp src/index.html "src/$playlist.html"
   fi
 done
